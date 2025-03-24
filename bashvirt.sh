b0fa45e6720c6fd0c700bab9365924e80cca291f
undus5 <undus5.curtsy947@passinbox.com>
2025-03-24 16:48:43 +0800

    improve fallback compatibility

--- bashvirt.sh
+++ bashvirt.sh
@@ -1,36 +1,62 @@
 #!/usr/bin/env bash
 
-err() {
-    printf "%s\n" "${1}" >&2; exit 1
+# exit on error
+set -e
+
+printerr() {
+    printf "${@}" >&2; exit 1
 }
 
-[[ -z ${_sdir} ]] && err "_sdir is undefined, \
-    set with \`_sdir=\$\(dirname \$\(realpath \$\{BASH_SOURCE[0]\}\)\)\`"
+_caller_path=$(realpath $0)
+_source_path=$(realpath ${BASH_SOURCE[0]})
+
+[[ "${_caller_path}" == "${_source_path}" ]] && \
+    printerr "do not run this script directly, source it\n"
+
+[[ -z "${_vmdir}" ]] && printerr "_vmdir is undefined\n"
 
 #################################################################################
 # Disk Image
 #################################################################################
 
-[[ -z ${_disk} ]] && _disk=${_sdir}/disk.qcow2
+[[ -z "${_disk_file}" ]] && _disk_file=${_vmdir}/disk.qcow2
+[[ -z "${_disk_drive}" ]] && _disk_drive="ide"
+
+case "${_disk_drive}" in
+    ide)
+        _diskdev="ide-hd"
+        ;;
+    virtio)
+        _diskdev="virtio-blk-pci"
+        ;;
+    *)
+        printerr "_disk_drive only support: <ide|virtio>\n"
+        ;;
+esac
+
+_disk_drives="\
+    -drive file=${_disk_file},if=none,id=disk0 \
+    -device ${_diskdev},drive=disk0,bootindex=1"
 
 qemu_disk_check() {
-    if [[ ! -f ${_disk} ]]; then
-        err "file not found: $(basename ${_disk}), create via command: \
-            \`qemu-img create -f qcow2 $(basename ${_disk}) -o nocow=on 40G\`"
+    if [[ ! -f ${_disk_file} ]]; then
+        printf "file not found: $(basename ${_disk_file})\n"
+        printerr "create via: \`qemu-img create -f qcow2 $(basename ${_disk_file}) -o nocow=on 40G\`\n"
     fi
 }
 
 #################################################################################
-# UEFI / BIOS
+# BIOS / UEFI
 #################################################################################
 
-[[ -z ${_bootmode} ]] && _bootmode="uefi"
+[[ -z ${_boot_mode} ]] && _boot_mode="bios"
 
-if [[ "${_bootmode}" == "uefi" ]]; then
+if [[ "${_boot_mode}" == "uefi" ]]; then
     _ovmf_ro=/usr/share/edk2/x64/OVMF_CODE.secboot.4m.fd
-    _ovmf_var=${_sdir}/OVMF_VARS.4m.fd
+    _ovmf_var=${_vmdir}/OVMF_VARS.4m.fd
     if [[ ! -f ${_ovmf_var} ]]; then
-        err "$(basename ${_ovmf_var}) not found, copy from: /usr/share/edk2/x64/"
+        printf "file not found: $(basename ${_ovmf_var})\n"
+        printerr "copy from: /usr/share/edk2/x64/OVMF_VARS.4m.fd\n"
     fi
     _uefi_drives="\
         -drive if=pflash,format=raw,readonly=on,file=${_ovmf_ro} \
@@ -38,37 +64,11 @@ if [[ "${_bootmode}" == "uefi" ]]; then
 fi
 
 #################################################################################
-# Graphic Card
-#################################################################################
-
-[[ -z ${_gpu} ]] && _gpu=virtio
-case ${_gpu} in
-    std|qxl)
-        _gpu_device="-vga ${_gpu}"
-        ;;
-    virtio)
-        _gpu_device="-device virtio-vga-gl"
-        ;;
-esac
-
-#################################################################################
-# BootCD
-#################################################################################
-
-if [[ -n ${_bootiso} ]]; then
-    [[ -f ${_bootiso} ]] || err "file not found: ${_bootiso}"
-    _bootcd="\
-        -drive file=${_bootiso},media=cdrom,if=none,id=cd0 \
-        -device ide-cd,drive=cd0,bootindex=0"
-fi
-
-#################################################################################
 # TPM
 #################################################################################
 
-_tpm_dir=${_sdir}
-_tpm_pid=${_tpm_dir}/swtpm.pid
-_tpm_sock=${_tpm_dir}/swtpm.sock
+_tpm_pid=${_vmdir}/swtpm.pid
+_tpm_sock=${_vmdir}/swtpm.sock
 
 if [[ "${_tpm_on}" == "yes" ]]; then
     _tpm_devices="\
@@ -77,15 +77,14 @@ if [[ "${_tpm_on}" == "yes" ]]; then
 fi
 
 is_pid_swtpm() {
-    ps -o command= -p ${1} | grep -q ${_tpm_dir}
+    ps -o command= -p ${1} | grep -q swtpm
 }
 
 init_swtpm() {
-    command -v swtpm &>/dev/null || err "swtpm: command not found"
-    [[ -d ${_tpm_dir} ]] || err "$(basename ${_tpm_dir}): directory not found"
+    command -v swtpm &>/dev/null || printerr "swtpm: command not found\n"
     if [[ ! -f ${_tpm_pid} ]] || [[ ! $(is_pid_swtpm $(cat ${_tpm_pid})) ]]; then
         swtpm socket --tpm2 \
-            --tpmstate dir=${_tpm_dir} \
+            --tpmstate dir=${_vmdir} \
             --ctrl type=unixio,path=${_tpm_sock} \
             --pid file=${_tpm_pid} &
     fi
@@ -98,31 +97,98 @@ kill_swtpm() {
 }
 
 #################################################################################
+# BootCD
+#################################################################################
+
+if [[ -n ${_boot_iso} ]]; then
+    [[ ! -f ${_boot_iso} ]] || printerr "file not found: ${_boot_iso}\n"
+    _bootcd="\
+        -drive file=${_boot_iso},media=cdrom,if=none,id=cd0 \
+        -device ide-cd,drive=cd0,bootindex=0"
+fi
+
+#################################################################################
+# Graphic Card
+#################################################################################
+
+[[ -z "${_gpu_drive}" ]] && _gpu_drive=std
+case "${_gpu_drive}" in
+    std|qxl)
+        _gpu_device="-vga ${_gpu_drive}"
+        ;;
+    virtio)
+        _gpu_device="-device virtio-vga-gl"
+        ;;
+    *)
+        printerr "_gpu_drive only support: <std|qxl|virtio>\n"
+        ;;
+esac
+
+#################################################################################
+# Network Card
+#################################################################################
+
+[[ -z "${_nic_drive}" ]] && _nic_drive="e1000"
+
+case "${_nic_drive}" in
+    e1000)
+        _nic_model="e1000"
+        ;;
+    virtio)
+        _nic_model="virtio-net-pci"
+        ;;
+    *)
+        printerr "_nic_drive only support: <e1000|virtio>\n"
+        ;;
+esac
+
+[[ -z "${_nic_mode}" ]] && _nic_mode="user"
+
+gen_mac_addr() {
+    printf "$(basename ${_vmdir})" | sha256sum |\
+        awk -v offset="$(( ${1} + 7 ))" '{ printf "52:54:%s:%s:%s:%s\n", \
+        substr($1,1,2), substr($1,3,2), substr($1,5,2), substr($1,offset,2) }'
+}
+
+case "${_nic_mode}" in
+    user)
+        _nic_devices="-nic user,model=${_nic_model},mac=$(gen_mac_addr 0)"
+        ;;
+    br0br1)
+        ip link show | grep -E 'br0|br1' &>/dev/null || \
+            printerr "network bridge not found: br0 or br1"
+        _nic_devices="\
+            -nic bridge,br=br0,model=${_nic_model},mac=$(gen_mac_addr 0) \
+            -nic bridge,br=br1,model=${_nic_model},mac=$(gen_mac_addr 1)"
+        ;;
+    *)
+        printerr "_nic_mode only support: <user|br0br1>\n"
+        ;;
+esac
+
+#################################################################################
 # QEMU Options builder
 #################################################################################
 
-[[ -z ${_mem} ]] && _mem=4G
+[[ -z ${_memory} ]] && _memory=2G
 # check CPU cores with `lscpu` command, or `cat /etc/proc/cpuinfo`
-[[ -z ${_cpus} ]] && _cpus=4
-_qemu_pid=${_sdir}/qemu.pid
-_monitor_sock=${_sdir}/monitor.sock
+[[ -z ${_cpu_cores} ]] && _cpu_cores=2
+_qemu_pid=${_vmdir}/qemu.pid
+_monitor_sock=${_vmdir}/monitor.sock
 
 gen_mac_addr() {
-    printf "$(basename ${_sdir})" | sha256sum |\
+    printf "$(basename ${_vmdir})" | sha256sum |\
         awk -v offset="$(( ${1} + 7 ))" '{ printf "52:54:%s:%s:%s:%s\n", \
         substr($1,1,2), substr($1,3,2), substr($1,5,2), substr($1,offset,2) }'
 }
 
 _qemu_options="\
-    -enable-kvm -cpu host -smp ${_cpus} -m ${_mem} -machine q35 \
-    -drive file=${_disk},if=none,id=disk0 \
-    -device virtio-blk-pci,drive=disk0,bootindex=1 \
-    -nic bridge,br=br0,model=virtio-net-pci,mac=$(gen_mac_addr 0) \
-    -nic bridge,br=br1,model=virtio-net-pci,mac=$(gen_mac_addr 1) \
-    -audiodev pipewire,id=snd0 -device ich9-intel-hda -device hda-duplex,audiodev=snd0 \
-    -monitor unix:${_monitor_sock},server,nowait -device qemu-xhci \
+    -enable-kvm -cpu host -smp ${_cpu_cores} -m ${_memory} -machine q35 \
+    -audiodev pa,id=snd0 -device ich9-intel-hda -device hda-duplex,audiodev=snd0 \
+    -monitor unix:${_monitor_sock},server,nowait \
+    -device qemu-xhci -pidfile ${_qemu_pid} \
     -display sdl,gl=on,full-screen=on ${_gpu_device} \
-    -pidfile ${_qemu_pid} ${_uefi_drives} ${_bootcd} ${_tpm_devices}"
+    ${_uefi_drives} ${_tpm_devices} ${_disk_drives} ${_bootcd} ${_nic_devices}"
 
 #################################################################################
 # QEMU start
@@ -140,8 +206,8 @@ qemu_err_fallback() {
 
 qemu_running_check() {
     if [[ -f ${_qemu_pid} ]]; then
-        _proc_comm=$(cat ${_qemu_pid} | xargs -I{} ps -p {} -o command=)
-        [[ "${_proc_comm}" =~ "qemu-system-x86_64" ]] && err "vm already running"
+        _proc_comm=$(cat ${_qemu_pid} | xargs -I{} ps -o command= -p {})
+        [[ "${_proc_comm}" =~ "qemu-system-x86_64" ]] && printerr "vm already running\n"
     fi
 }
 
@@ -150,7 +216,7 @@ qemu_start() {
     qemu_disk_check
     trap 'qemu_err_fallback; exit 1' ERR
     qemu_prerequisite
-    qemu-system-x86_64 ${_qemu_options} ${_qemu_options_ext} 2>>${_sdir}/qemu_err.log
+    qemu-system-x86_64 ${_qemu_options} ${_qemu_options_ext} 2>>${_vmdir}/qemu_err.log
 }
 
 #################################################################################
@@ -161,7 +227,7 @@ monitor_connect() {
     socat -,echo=0,icanon=0 unix-connect:${_monitor_sock}
 }
 
-monitor_invoke() {
+monitor_exec() {
     [[ -S ${_monitor_sock} ]] && echo "${@}" | \
         socat - UNIX-CONNECT:${_monitor_sock} | \
         tail --lines=+2 | grep -v '^(qemu)'
@@ -169,21 +235,21 @@ monitor_invoke() {
 
 usb_attach() {
     local _devid=$(echo "${1}" | tr -d [:space:])
-    [[ "${_devid}" =~ ^[a-z0-9]{4}:[a-z0-9]{4}$ ]] || err "invalid device ID"
+    [[ "${_devid}" =~ ^[a-z0-9]{4}:[a-z0-9]{4}$ ]] || printerr "invalid device ID\n"
     local _vendid=$(echo "${_arg}" | cut -d : -f 1)
     local _prodid=$(echo "${_arg}" | cut -d : -f 2)
-    monitor_invoke \
+    monitor_exec \
         "device_add usb-host,vendorid=0x${_vendid},productid=0x${_prodid},id=${_devid}"
 }
 
 usb_detach() {
     local _devid=$(echo "${1}" | tr -d [:space:])
-    [[ "${_devid}" =~ ^[a-z0-9]{4}:[a-z0-9]{4}$ ]] || err "invalid device ID"
-    monitor_invoke "device_del ${_devid}"
+    [[ "${_devid}" =~ ^[a-z0-9]{4}:[a-z0-9]{4}$ ]] || printerr "invalid device ID\n"
+    monitor_exec "device_del ${_devid}"
 }
 
 usb_list() {
-    monitor_invoke "info usb"
+    monitor_exec "info usb"
 }
 
 #################################################################################
@@ -194,13 +260,13 @@ print_help() {
 cat << EOB
 usage: $(basename $0) [actions]
 actions:
-                            boot normally without actions
+                            boot virtual machine normally without arguments
     usb-attach <device_id>  passthrough usb device to virtual machine
-    usb-detach <device_id>
+    usb-detach <device_id>  detach usb device
     usb-list                list attached devices
-    invoke                  send command to qemu monitor
-    conn                    connect qemu monitor
-    -h, --help              help info
+    monitor-exec            send command to qemu monitor
+    monitor-connect         connect qemu monitor
+    -h, --help, help        help info
 device_id:
     looks like "1d6b:0002", get from command \`lsusb\`, 
 EOB
@@ -221,14 +287,14 @@ case ${1} in
     usb-list)
         usb_list
         ;;
-    invoke)
+    monitor-exec)
         shift
-        monitor_invoke ${@}
+        monitor_exec ${@}
         ;;
-    conn)
+    monitor-connect)
         monitor_connect
         ;;
-    -h|--help)
+    -h|--help|help)
         print_help
         ;;
     *)
--- example.sh
+++ example.sh
@@ -1,20 +1,44 @@
 #!/usr/bin/bash
 
-_sdir=$(dirname $(realpath ${BASH_SOURCE[0]}))
+# _vmdir is required, to store all the runtime files
+# e.g. _vmdir=$(dirname $(realpath ${BASH_SOURCE[0]}))
+_vmdir=path/to/vm_runtime_dir
 
-_mem=4G
-# check CPU cores with `lscpu` command, or `cat /etc/proc/cpuinfo`
-_cpus=4
-#_bootmode=[bios|uefi], default is uefi
-_bootmode=uefi
-#_gpu=[std|qxl|virtio], default is virtio
-_gpu=virtio
-#_disk=${_sdir}/disk.qcow2
+# disk image file, default is ${_vmdir}/disk.qcow2
+# create via command `qemu-img create -f qcow2 disk.qcow2 -o nocow=on 40G`
+#_disk_file=${_vmdir}/disk.qcow2
 
-# _bootiso=path/to/windows.iso
-# _secondiso=path/to/virtio.iso
-# [[ -f ${_secondiso} ]] || err "file not found: ${_secondiso}"
-# _qemu_options_ext="${_qemu_options_ext} -drive file=${_secondiso},media=cdrom"
+# cpu cores, default is 2
+# check cpu cores with command `lscpu`, or `cat /etc/proc/cpuinfo`
+#_cpu_cores=4
 
-source ${_sdir}/bashvirt.sh
+# memory, default is 2G
+#_memory=4G
+
+# boot mode [bios|uefi], default is bios
+#_boot_mode=uefi
+
+# enable tpm [yes|no], default is no
+#_tpm_on=yes
+
+# boot from iso
+#_boot_iso=path/to/windows.iso
+
+# add more non-boot iso:
+#_secondiso=path/to/virtio.iso
+#_qemu_options_ext="${_qemu_options_ext} -drive file=${_secondiso},media=cdrom"
+
+# network cards mode, [user|br0br1], default is user
+#_nic_mode=br0br1
+
+# network drive [e1000|virtio], default is e1000
+#_nic_drive=virtio
+
+# graphic card, std or qxl or virtio, optional, default is std
+#_gpu_drive=virtio
+
+# disk drive [ide|virtio], default is ide
+#_disk_drive=virtio
+
+source path/to/bashvirt.sh
 
